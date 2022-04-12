f83d1abaf212fba4d9f90344d49d993e56b77bfe
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-04-12 23:27:47 +0200

    html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -21,4 +22,11 @@ module.exports = {
             }
         ]
     },
+    plugins:[
+        new HtmlWebPackPlugin({
+            inject: true,
+            template:'./public/index.html',
+            filename: './index.html'
+        })
+    ]
 } 
