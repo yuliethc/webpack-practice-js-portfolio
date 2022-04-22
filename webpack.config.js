c80f6a11b47f749c7754617906139b3bba2bc71f
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-04-22 10:27:41 +0200

    image loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
                     'css-loader',
                     'stylus-loader'
                 ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
             }
         ]
     },
